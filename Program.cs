9144750ececd8b49ac599d2433b82cc5c3d944fd
ZavarkoZeno <zavarkoz@kkszki.hu>
2024-10-18 10:48:11 +0200

    Elkezdtem a feladatot

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szamonkeres2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int positiveSum = 0;
+            int negativeSum = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Adja meg a {i + 1} számot: ");
+                if (int.TryParse(Console.ReadLine(), out int number))
+                {
+
+                }
+            }
+        }
+    }
+}
